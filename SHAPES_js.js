1e0991931daaec456632d95fd989d03431408320
AdrianFigueroaRodriguez <adrianfigueroarodriguez@outlook.com>
2018-11-10 00:24:22 -0700

    update  10-11

--- SHAPES_js.js
+++ SHAPES_js.js
@@ -2,37 +2,28 @@ function mostrarContenidoA() {
 	document.getElementById("DropDownMenuA").classList.toggle("show");
 }
 
-window.onclick = function(e) {
-  if (!e.target.matches('.DropDownBtnA')) {
-    var myDropdownA = document.getElementById("DropDownMenuA");
-      if (myDropdownA.classList.contains('show')) {
-        myDropdownA.classList.remove('show');
-      }
-  }
-}
-
 function mostrarContenidoP() {
 	document.getElementById("DropDownMenuP").classList.toggle("show");
 }
 
-window.onclick = function(e) {
-  if (!e.target.matches('.DropDownBtnP')) {
-    var myDropdownP = document.getElementById("DropDownMenuP");
-      if (myDropdownP.classList.contains('show')) {
-        myDropdown.classList.remove('show');
-      }
-  }
-}
 
 function mostrarContenidoV() {
 	document.getElementById("DropDownMenuV").classList.toggle("show");
 }
 
 window.onclick = function(e) {
-  if (!e.target.matches('.DropDownBtnV')) {
-    var myDropdownV = document.getElementById("DropDownMenuV");
-      if (myDropdownV.classList.contains('show')) {
-        myDropdownV.classList.remove('show');
-      }
+  if (!e.target.matches('.DropDownBtnA') &&
+	  !e.target.matches('.DropDownBtnP') &&
+	  !e.target.matches('.DropDownBtnV')) {
+	    var myDropdownA = document.getElementById("DropDownMenuA");
+		var myDropdownP = document.getElementById("DropDownMenuP");
+		var myDropdownV = document.getElementById("DropDownMenuV");
+	      if (myDropdownA.classList.contains('show')) {
+	      	myDropdownA.classList.remove('show');
+	      } else if(myDropdownP.classList.contains('show')){
+	      	myDropdownP.classList.remove('show');
+	      } else{
+	      	myDropdownV.classList.remove('show');
+	      }
   }
 }
